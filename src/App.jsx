9c8e2727d8e80c95ec10dedc7eb49a224233a1b6
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-06-24 14:52:15 -0400

    add createBrowserRouter and RouterProvider from the react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import { MainPage } from "./components/MainPage";
+import { SuccessPage } from "./components/SuccessPage";
+
+const router = createBrowserRouter([
+	{ path: "/", element: <MainPage /> },
+	{ path: "/success", element: <SuccessPage /> },
+]);
 
 function App() {
 	return (
 		<>
-			<MainPage></MainPage>
+			<RouterProvider router={router} />
 		</>
 	);
 }
