e39a182085d503f28f9d3d852e013a2f5fdaeb8b
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-06-24 20:15:15 -0400

    add email context provider to MainPage and SuccessPage path

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,27 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { MainPage } from "./components/MainPage";
 import { SuccessPage } from "./components/SuccessPage";
+import { EmailContextProvider } from "./components/EmailContext";
 
 const router = createBrowserRouter([
-	{ path: "/", element: <MainPage /> },
-	{ path: "/success", element: <SuccessPage /> },
+	{
+		path: "/",
+		element: (
+			<EmailContextProvider>
+				{" "}
+				<MainPage />
+			</EmailContextProvider>
+		),
+	},
+	{
+		path: "/success",
+		element: (
+			<EmailContextProvider>
+				{" "}
+				<SuccessPage />{" "}
+			</EmailContextProvider>
+		),
+	},
 ]);
 
 function App() {
